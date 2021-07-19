f25f6c4d390f16e09732e93ca223bdcb95694178
Muxed0 <muxed0@protonmail.com>
2021-07-19 15:00:41 -0400

    Implemented ship selection

---
+++ game/entities/player/ship_data.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var ship_type = 0
+
--- game/ui/main_menu/main_menu.gd
+++ game/ui/main_menu/main_menu.gd
@@ -1,4 +1,15 @@
 extends Control
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://world/battle/battle_stage_1.tscn")
+	if $"/root/ShipData".ship_type != 0:
+		get_tree().change_scene("res://world/battle/battle_stage_1.tscn")
+
+
+func _on_ship1_select_button_button_down():
+	$"/root/ShipData".ship_type = 1
+	$ship2_select_button.pressed = false
+
+
+func _on_ship2_select_button_button_down():
+	$"/root/ShipData".ship_type = 2
+	$ship1_select_button.pressed = false
