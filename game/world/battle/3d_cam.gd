e7393316f88fd61361de9c517201de13f0c5d500
Muxed0 <muxed0@protonmail.com>
2021-07-22 14:43:39 -0400

    Added angular damping. Much better.

--- game/world/battle/3d_cam.gd
+++ game/world/battle/3d_cam.gd
@@ -1,17 +1,17 @@
 extends Camera
 
-export var SPEED_SCALE = 0.000001
+export var SPEED_SCALE = 0.00001
 
 var radius = 0
 var rot_speed = 0
-var previous_rotation = Vector2(0,0)
+var previous_pos = Vector3(0,0,0)
 var wrap_counter = Vector2(0,0)
 
 var a = 0
 var b = 0
 var z_quad = Vector2(0,0)
 var z = 0
-var midsection_pos = Vector2(0,0)
+var midsection_pos = Vector3(0,0,0)
 
 var velocity_basis = Basis(Vector3(1,0,0),Vector3(0,1,0),Vector3(0,0,1))
 
@@ -39,15 +39,25 @@ func _process(delta):
 	translation.x = radius * cos(rotation.x) * sin(rotation.y)
 	translation.z = radius * cos(rotation.x) * cos(rotation.y)
 	
+	#Solving for intersection with front half of the equatior and positioning red orb
 	a = velocity_basis.y.x
 	b = velocity_basis.y.z
 	z_quad.x = a*a + b*b
 	z_quad.y = radius*radius * a*a
 	z = sqrt(4 * z_quad.x * z_quad.y)/(2 * z_quad.x)
 	midsection_pos.x = -(z * b)/a
-	midsection_pos.y = z
-	$"../debug_indicator".translation = Vector3(midsection_pos.x,0,midsection_pos.y)
+	midsection_pos.z = z
+	$"../debug_indicator".translation = midsection_pos
+	
+	if translation.y * previous_pos.y < 0 and (translation-midsection_pos).length() < 1:
+		if previous_pos.y < 0:
+			print("down -> up")
+		else:
+			print("up -> down")
+		wrap_counter.x += 1
+		print(wrap_counter)
 
+	previous_pos = translation
 	$"../debug_pill".transform.basis = velocity_basis
 	
 	if delta != 0:
